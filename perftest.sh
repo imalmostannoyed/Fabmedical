06c0a05a2530d877417c046cb90d834fb8a4f9f5
imalmostannoyed <94970220+imalmostannoyed@users.noreply.github.com>
2021-11-24 16:13:31 +0000

    Update perftest.sh

--- perftest.sh
+++ perftest.sh
@@ -1,6 +1,6 @@
-host="fabmedical-[SUFFIX].documents.azure.com"
-username="fabmedical-[SUFFIX]"
-password="[PASSWORD]"
+host="fabmedical-496129.mongo.cosmos.azure.com"
+username="fabmedical-496129"
+password="vDBQDpA291qaYQP8sOgygKkRruP9WGLPjNwq4DYCjaBraEx9CduOMrARTtyy9YKGEYj5HTXEuFV72ch8Ydc4WA=="
 dbname="contentdb"
 port="10255"
 
